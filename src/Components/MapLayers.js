d99fe96158d0e059b2a75758309938cc3fa7163c
azrealthief <azrealthief@hotmail.com>
2022-04-01 13:33:23 +0800

    updated the 5 json files into the src folder and updated ViewMap, maplayer blank now

--- src/Components/MapLayers.js
+++ src/Components/MapLayers.js
@@ -1,47 +1,58 @@
-import * as React from "react";
-import { useState } from 'react'
-import { useMapEvents, TileLayer, LayersControl, GeoJSON } from 'react-leaflet'
+
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import Gyms from '../gyms-sg-geojson.geojson'
+import L from "leaflet";
 
-const MapLayers = () => {
-    // Set all the border data to state array:
-    const [borderData, setBorderData] = useState([Gyms])
 
-    // Call useMapEvents:
-    const map = useMapEvents({
-      // Use leaflet map event as the key and a call back with the 
-      // map method as the value:
-      zoomend: () => {
-        // Get the zoom level once zoom ended:
-        console.log(map.getZoom())
-      },
-      moveend: () => {
-      // Get bounds once move has ended:
-        console.log(map.getBounds())
-      }
-    })
-  
-  
-    return (
-        <>
-      <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="Basic Map">
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="Topo Map">
-          <TileLayer
-            attribution='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-            url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-          />
-        </LayersControl.BaseLayer>
-        
-      </LayersControl>
-    </>
-    )
+const blackMarker = new L.icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/25/25613.png",
+  iconSize: [25, 25],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40]
+});
+export default function MapLayers() {
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const [bbox, setBbox] = useState([]);
+
+    const map = useMap();
+
+    useEffect(() => {
+      map.locate().on("locationfound", function (e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+        const radius = e.accuracy;
+        const circle = L.circle(e.latlng, radius);
+        circle.addTo(map);
+        setBbox(e.bounds.toBBoxString().split(","));
+      });
+    }, [map]);
+
+    return position === null ? null : (
+      <Marker position={position} icon={blackMarker}>
+        <Popup>
+          You are here. <br />
+          Map bbox: <br />
+          <b>Southwest lng</b>: {bbox[0]} <br />
+          <b>Southwest lat</b>: {bbox[1]} <br />
+          <b>Northeast lng</b>: {bbox[2]} <br />
+          <b>Northeast lat</b>: {bbox[3]}
+        </Popup>
+      </Marker>
+    );
   }
 
-export default MapLayers
+  return (
+    <MapContainer
+      scrollWheelZoom
+      style={{ height: "100vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker />
+    </MapContainer>
+  );
+}
